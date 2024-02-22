25c931f916f6c224cf0eac885cf23de06fb2cb26
David Doria <daviddoria@Davids-M1-MacBook-Pro.local>
2024-02-22 15:56:14 -0800

    Fixed Sidebar bug

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from "next";
 import { IBM_Plex_Sans } from "next/font/google";
-import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ClerkProvider } from "@clerk/nextjs";
+
+import "./globals.css";
 
 const IBMPlex = IBM_Plex_Sans({
   subsets: ["latin"],
@@ -12,7 +13,7 @@ const IBMPlex = IBM_Plex_Sans({
 
 export const metadata: Metadata = {
   title: "Imaginify",
-  description: "AI powered image manipulator",
+  description: "AI-powered image generator",
 };
 
 export default function RootLayout({
@@ -21,7 +22,11 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider appearance={{ variables: { colorPrimary: "#624cf5" } }}>
+    <ClerkProvider
+      appearance={{
+        variables: { colorPrimary: "#624cf5" },
+      }}
+    >
       <html lang="en">
         <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
           {children}
--- components/shared/Sidebar.tsx
+++ components/shared/Sidebar.tsx
@@ -81,8 +81,7 @@ const Sidebar = () => {
           </SignedIn>
           <SignedOut>
             <Button asChild className="button bg-purple-gradient bg-cover">
-              <Link href="/sign-in" />
-              Login
+              <Link href="/sign-in">Login</Link>
             </Button>
           </SignedOut>
         </nav>
